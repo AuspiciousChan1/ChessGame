8c63f3e307e052d0230abc22a74bbba1a4d49078
AuspiciousChan <AuspiciousChan@163.com>
2026-01-23 20:38:34 +0800

    feat: 支持简陋版本的Network测试，跑通基本逻辑

--- testtool/src/main/java/com/chenjili/chessgame/testtool/testpage/networkservice/NetworkServiceTestActivity.kt
+++ testtool/src/main/java/com/chenjili/chessgame/testtool/testpage/networkservice/NetworkServiceTestActivity.kt
@@ -4,44 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.chenjili.chessgame.testtool.testpage.networkservice.ui.NetworkServiceTestViewModel
+import com.chenjili.chessgame.testtool.testpage.networkservice.ui.networkServiceTestScreen
 import com.chenjili.chessgame.testtool.testpage.networkservice.ui.theme.ChessGameTheme
 
 class NetworkServiceTestActivity : ComponentActivity() {
+    private val viewModel: NetworkServiceTestViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ChessGameTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            val state by viewModel.state.collectAsState()
+            networkServiceTestScreen(state, onIntent = viewModel::processIntent)
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Network Service Test Page",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ChessGameTheme {
-        Greeting("Android")
     }
 }
